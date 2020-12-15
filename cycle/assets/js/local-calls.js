e8cf403ba4cfa69ec82547f537c9fdd2415c9527
agwarren2 <akwarren2@ualr.edu>
2020-12-15 06:49:42 +0000

    finalized work

---
+++ cycle/assets/js/local-calls.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+// Your code goes here...
+
+});
+
+$('.gallery').cycle({
+		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+	
